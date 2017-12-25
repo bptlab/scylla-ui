929dd1cea7f9d85e675d3dd8aee10dcf185954a9
Marc André Freiheit <marcandre@freiheit.software>
2017-12-25 21:44:22 +0100

    Add 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { 404Component } from './misc/404/404.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    404Component
   ],
   imports: [
     BrowserModule
