e81b9cfd65b6271ccab54c0a79aee91ffb375baa
Marc André Freiheit <marcandre@freiheit.software>
2017-12-25 23:30:37 +0100

    Add header bar with links to GitHub #1

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+}from '@angular/material';
+
 import { FileSelectionComponent } from './file-selection/file-selection.component';
 import { HeaderBarComponent } from './menu/header-bar/header-bar.component';
 import { FooterBarComponent } from './menu/footer-bar/footer-bar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [FileSelectionComponent, HeaderBarComponent, FooterBarComponent],
   exports: [
