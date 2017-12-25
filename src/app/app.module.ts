902434d9a4e3dc815abc8577e092711d35912a7f
Marc André Freiheit <marcandre@freiheit.software>
2017-12-25 21:58:55 +0100

    Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LandingModule } from './landing/landing.module';
 import { StatisticsModule } from './statistics/statistics.module';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './misc/not-found/not-found.component';
+import { FileSelectionComponent } from './landing/file-selection/file-selection.component';
 
+export const ROUTES = [
+  { path: 'files', component: FileSelectionComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/files' },
+  { path:'**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
@@ -15,7 +22,8 @@ import { NotFoundComponent } from './misc/not-found/not-found.component';
   imports: [
     BrowserModule,
     LandingModule,
-    StatisticsModule
+    StatisticsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
