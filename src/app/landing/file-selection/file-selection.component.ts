8fbc744d4bccdb06ef1e894d5368fd3eb40cbe55
bisbaldi <annika.baldi@googlemail.com>
2018-01-04 15:07:39 +0100

    Add first draft of file reader

--- src/app/landing/file-selection/file-selection.component.ts
+++ src/app/landing/file-selection/file-selection.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-file-selection',
   templateUrl: './file-selection.component.html',
-  styleUrls: ['./file-selection.component.css']
+  styleUrls: ['./file-selection.component.css'],
 })
-export class FileSelectionComponent implements OnInit {
+export class FileSelectionComponent {
 
-  constructor() { }
+  dragging = false;
+  loaded = false;
+  fileLoaded = false;
+  fileSrc = '';
 
-  ngOnInit() {
+  handleDragEnter() {
+    this.dragging = true;
+  }
+
+  handleDragLeave() {
+    this.dragging = false;
+  }
+
+  handleDrop(e) {
+    e.preventDefault();
+    this.dragging = false;
+    this.handleInputChange(e);
+  }
+
+  handleImageLoad() {
+    this.fileLoaded = true;
+  }
+
+  handleInputChange(e) {
+    const file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+
+    const pattern = /-*.bpmn/;
+    const reader = new FileReader();
+
+    /*if (!file.type.match(pattern)) {
+      alert('invalid format blubber');
+      return;
+    }*/
+
+    this.loaded = false;
+
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+
+  _handleReaderLoaded(e) {
+    const reader = e.target;
+    this.fileSrc = reader.result;
+    this.loaded = true;
   }
 
 }
