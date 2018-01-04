ac6d3ab04374621eda2bfe6493ab4c4fd2cfb5c9
Marc André Freiheit <marcandre@freiheit.software>
2018-01-04 14:31:06 +0100

    Add routing module for app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LandingModule } from './landing/landing.module';
 import { StatisticsModule } from './statistics/statistics.module';
-import { Router, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './misc/not-found/not-found.component';
 import { LandingComponent } from './landing/landing.component';
 
-export const ROUTES = [
-  { path: 'landing', component: LandingComponent },
-  { path: '', pathMatch: 'full', redirectTo: '/landing' },
-  { path: '**', component: NotFoundComponent }
-];
 
 @NgModule({
   declarations: [
@@ -23,7 +18,7 @@ export const ROUTES = [
     BrowserModule,
     LandingModule,
     StatisticsModule,
-    RouterModule.forRoot(ROUTES)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
