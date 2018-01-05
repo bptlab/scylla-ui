5c486eb75e54bd1b63380acfe9447331c3489e90
bisbaldi <annika.baldi@googlemail.com>
2018-01-05 12:57:40 +0100

    Cut unnecessary file encoding information when storing the files

--- src/app/services/file-sharing/file-sharing.service.ts
+++ src/app/services/file-sharing/file-sharing.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FileSharingService {
-  private _bpmn: File;
-  private _xes: String;
-  private _JSON: JSON;
+  private _bpmn: string;
+  private _xes: string;
+  private _json: string;
 
-  // stores Data in Base64 encoding
-  set(fileFormat: String, fileSrc) {
-    this['_' + fileFormat] = fileSrc;
+  // stores data in Base64 encoding
+  set(fileFormat: string, fileSrc: string) {
+    this['_' + fileFormat] = this.cutFileEncoding(fileSrc);
   }
 
-  get(fileFormat: String) {
+  get(fileFormat: string) {
     return this['_' + fileFormat];
+  }
+
+  cutFileEncoding(fileSrc: string) {
+    const splitString = fileSrc.split(',');
+    return splitString[1];
   }
 
 }
