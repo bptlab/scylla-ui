54f75af46e86b6e90781da2132e3ab5345514fd8
bisbaldi <annika.baldi@googlemail.com>
2018-01-08 12:03:23 +0100

    FIx linting

--- src/app/statistics/bpmn-viewer/bpmnViewer.ts
+++ src/app/statistics/bpmn-viewer/bpmnViewer.ts
@@ -16,4 +16,4 @@ import 'bpmn-js-properties-panel/lib/provider/bpmn/BpmnPropertiesProvider.js';
 // import 'bpmn-js-properties-panel/styles/properties.less';
 import 'bpmn-js/assets/bpmn-font/css/bpmn.css';
 import 'bpmn-js/assets/bpmn-font/css/bpmn-embedded.css';
-import 'diagram-js/assets/diagram-js.css'
+import 'diagram-js/assets/diagram-js.css';
